2abf79449fec8e1fa14ff45f021b3d60b6b107b0
GAnzures <75755062+GAnzures@users.noreply.github.com>
2021-06-18 14:39:37 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,20 @@
+$(".aboutme").click(function() {
+   $(".about-Me").slideToggle();
+});
+
+$(".Posts").click(function() {
+   $(".posts-pic").slideToggle();
+});
+
+$(".Dwayne-title").click(function() {
+   $("body").css("background-color", "#7045ba");
+});
+
+$(".reverse").click(function() {
+   $("body").css("background-color", "lightgrey");
+});
+
+$("h2").click(function() {
+   $(".pictures").fadeToggle();
+});
+
